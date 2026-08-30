3455483027831982523a12a02bbe4c41dbfcff40
BX0830bluesea <osbornrose5301@gmail.com>
2026-08-30 23:07:13 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,23 +8,3 @@ myImage.onclick = () => {
   }
 };
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mutsumi is cute, ${myName}`;
-  }
-}
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mutsumi is cute, ${storedName}`;
-}
-myButton.onclick = function () {
-  setUserName();
-};
